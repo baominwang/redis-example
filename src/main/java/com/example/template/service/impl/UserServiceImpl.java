3adbcbfa8ec2e19a8df962d2e2da78a5c3f72753
baominwang <greencacti@163.com>
2022-06-02 11:26:37 +0800

    Add redis support

--- src/main/java/com/example/template/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/template/service/impl/UserServiceImpl.java
@@ -8,15 +8,22 @@ import com.example.template.dto.DescribeUsersRequest;
 import com.example.template.mapper.UserMapper;
 import com.example.template.repository.UserRepository;
 import com.example.template.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     @Override
     public String create(CreateUserRequest request) {
@@ -24,6 +31,12 @@ public class UserServiceImpl implements UserService {
         User user = UserMapper.INSTANCE.toDomain(request);
         String userId = IdUtil.getSnowflakeNextIdStr();
         user.setUserId(userId);
+
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        String key = "user:" + userId;
+        ops.set(key, user.getFirstName() + " " + user.getLastName());
+        String value = ops.get(key);
+        log.info("The value for key: {} is {}", key, value);
 
         // persist the user
         userRepository.save(user);
